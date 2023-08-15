50360caf692a4717bfe6347440ceda10b20fefe4
Alexhalo <alexblockchain.3115@gmail.com>
2023-08-15 16:39:52 +0100

    quick delete

--- rock_paper_scissor/src/configs.rs
+++
@@ -1,8 +0,0 @@
-pub const JSON_RPC_ENDPOINT: &str = "http://0.0.0.0:5050";
-pub const ACCOUNT_ADDRESS: &str =
-    "0x03ee9e18edc71a6df30ac3aca2e0b02a198fbce19b7480a63a0d71cbd76652e0"; // katana account 0
-pub const ACCOUNT_SECRET_KEY: &str =
-    "0x0300001800000000300000180000000000030000000000003006001800006600";
-pub const WORLD_ADDRESS: &str = "0x5b328933afdbbfd44901fd69a2764a254edbb6e992ae87cf958c70493f2d201";
-pub const DOJO_SYNC_INTERVAL: f32 = 0.5;
-pub const MODEL_NAME: &str = "model";
--- rock_paper_scissor/src/lib.cairo
+++
@@ -1,1 +0,0 @@
-mod not_test;
